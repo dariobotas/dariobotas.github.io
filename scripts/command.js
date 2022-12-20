50a416d1985ff5880c01de69d4d064d3256c5904
dariobotas <dario.botas@gmail.com>
2022-12-20 20:45:24 +0000

    criação da base para o terminal web

--- scripts/command.js
+++ scripts/command.js
@@ -1,1 +1,90 @@
-const youtube = "";
+const youtube = "https://";
+const facebook = "https://www.facebook.com/dario.botas/";
+const linkedin = "www.linkedin.com/in/dbotas";
+const github = "https://github.com/dariobotas";
+const bitbucket = "https://bitbucket.org/dabotas/";
+const email = "mailto:darioabotas@gmail.com";
+
+ajuda = [
+    "<br>",
+    '<span class="command">queme</span>      Quem é o DBotas?',
+    '<span class="command">quemsou</span>    Quem és tu?',
+    '<span class="command">videos</span>     Ver os meus vídeos do Youtube',
+    '<span class="command">projetos</span>   Os meus projetos de programação',
+    '<span class="command">social</span>     Redes sociais onde me podem encontrar',
+    '<span class="command">senha</span>      Encontra a senha',
+    '<span class="command">limpa</span>      Limpar o terminal',
+    '<span class="command">historia</span>   Ver todos os commands inseridos antes',
+    '<span class="command">lingua</span>     Escolher a língua',
+    '<span class="command">setup</span>      Mudança do formato/cores do terminal',
+    "<br>"
+];
+
+help = [
+    "<br>",
+    '<span class="command">whois</span>      Who is DBotas?',
+    '<span class="command">whoami</span>     Who are you?',
+    '<span class="command">videos</span>     View youtube videos',
+    '<span class="command">projects</span>   View coding projects',
+    '<span class="command">social</span>     Display social networks',
+    '<span class="command">secret</span>     Find my password',
+    '<span class="command">clear</span>      Clear everything written before',
+    '<span class="command">history</span>    Display previous commands',
+    '<span class="command">language</span>   Choose Language',
+    '<span class="command">setup</span>      Setup color/font terminal',
+    "<br>"
+];
+
+languages = [
+    '<span class="command">en</span>        English Language',
+    '<span class="command">pt</span>        Portuguese Language'
+];
+
+whois = [
+    "<br>",
+    "<br>"
+];
+
+whoami = [
+    "<br>",
+    "<br>"
+];
+
+projetos = [
+    "<br>",
+    '<span class="command">faculdade</span>  ',
+    '<span class="command">pessoais</span>   ',
+    "<br>"
+];
+
+projects = [
+    "<br>",
+    '<span class="command">school</span>     ',
+    '<span class="command">personal</span>   ',
+    "<br>"
+];
+
+social = [
+    "<br>",
+    "<br>"
+];
+
+secret = [
+    "<br>",
+    "<br>"
+];
+
+senha = [
+    "<br>",
+    "<br>"
+];
+
+setupPt = [
+    "<br>",
+    "<br>"
+];
+
+setupEn = [
+    "<br>",
+    "<br>"
+];
